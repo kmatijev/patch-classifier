3fab111e0de7d74d0f14bc2c822d6a91d2b5d785
@OJK0029 <karlo.mat19@gmail.com>
2026-04-10 15:31:09 +0200

    fix: removed heatmaps and heatmap generation

--- wsi_inference/__init__.py
+++
@@ -1,9 +0,0 @@
-# WSI Inference Module
-# Makes functions importable from this package
-
-from .classify_wsi import WSIClassifier, get_available_models
-
-__all__ = [
-    'WSIClassifier',
-    'get_available_models',
-]
